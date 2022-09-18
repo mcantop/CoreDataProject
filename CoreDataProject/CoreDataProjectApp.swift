079bb6a35edc6d02d7b2fab3c14e201e5d2aa24f
mcantop <maciejcantop@icloud.com>
2022-09-18 18:01:06 +0200

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Maciej on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
